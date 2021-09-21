197157ebf82d1b1fc8e4f6e536b914b8c1484d6e
Wilson Diaz <wdfiguereo@gmail.com>
2021-09-20 23:42:29 -0400

    6.21: fix notification clear handling

--- redux-anecdotes/src/components/Notification.js
+++ redux-anecdotes/src/components/Notification.js
@@ -7,14 +7,12 @@ const Notification = (props) => {
     padding: 10,
     borderWidth: 1
   }
-
-  const notification = props.notification
-
-  if (!notification) { return null }
+  
+  if (!props.notification.message) { return null }
 
   return (
     <div style={style}>
-      { notification }
+      { props.notification.message }
     </div>
   )
 }
--- redux-anecdotes/src/reducers/notificationReducer.js
+++ redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,25 +1,38 @@
-const initialState = 'Welcome to the anecdotes app!'
+const initialState = {
+  message: 'Welcome to the anecdotes app!',
+  prevId: null
+}
 
 const notificationReducer = (state = initialState, action) => {
   switch(action.type) {
     case 'SET_NEW': {
-      return action.data
+      clearTimeout(state.prevId)
+      return {
+        message: action.data.message,
+        prevId: action.data.clearId
+      }
     }
     case 'CLEAR':
-      return null
+      return {
+        message: null,
+        prevId: null
+      }
     default: return state
   }
 }
 
 export const setNotification = (message, seconds) => {
   return async dispatch => {
-    dispatch({
-      type: 'SET_NEW',
-      data: message
-    })
-    return setTimeout(() => {
+    const clearId = setTimeout(() => {
       dispatch(clearNotification())
     }, (seconds * 1000))
+    dispatch({
+      type: 'SET_NEW',
+      data: {
+        message,
+        clearId
+      }
+    })
   }
 }
 
